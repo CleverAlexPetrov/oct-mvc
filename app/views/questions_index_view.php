e481a4859ec165471d73d75ef45420a80a235702
Aleksandr <sckrudg@gmail.com>
2019-04-26 16:25:12 +0300

    add

--- app/views/questions_index_view.php
+++ app/views/questions_index_view.php
@@ -1,10 +1,10 @@
-<table>
+<table id="questions">
     <thead>
-        <tr>
-            <th>#</th>
-            <th>Name</th>
-            <th>Question</th>
-        </tr>
+	<tr>
+	    <th>#</th>
+	    <th>Name</th>
+	    <th>Question</th>
+	</tr>
     </thead>
     <tbody>
 
